a560c508b826bfaf85ee860706b90b08ff358525
student02 <student02@Codes-Air.fios-router.home>
2021-02-08 20:05:29 -0500

    start project

---
+++ src/test/java/hen/ashley/springbootcrudhibernateexample/SpringbootCrudHibernateExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package hen.ashley.springbootcrudhibernateexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudHibernateExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
