b26c2659b20b70eff5804ea3b84bfaa2f3d79659
student02 <student02@Codes-MacBook-Air.local>
2021-02-10 12:49:04 -0500

    complpeted monday

---
+++ src/main/java/JPA/model/Product.java
@@ -0,0 +1,79 @@
+package JPA.model;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private BigDecimal prices;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @CreationTimestamp
+    private Date updateAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrices() {
+        return prices;
+    }
+
+    public void setPrices(BigDecimal prices) {
+        this.prices = prices;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+}
---
+++ src/main/java/Service/ProductService.java
@@ -0,0 +1,19 @@
+package Service;
+
+
+import JPA.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    Product createProduct(Product product);
+
+    Product updateProduct(Product product);
+
+    List < Product > getAllProduct();
+
+    Product getProductById(long productId);
+
+    void deleteProduct(long id);
+
+}
